9b8d3ac213ffaf0f426c91d9e7ae11b94d0892f8
Anuj tuladhar <39491381+anoozchaos@users.noreply.github.com>
2023-10-05 10:31:53 +1100

    Add files via upload

---
+++ for loops.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#print numbers 1 to 10 using for loop 
+
+for i in range(1,11):
+    print (i)
+
+
+# In[6]:
+
+
+#calculate the sum of 1 to 100 
+sum = 0
+for i in range(1,101):
+      sum += i
+print (sum)    
+
+
+# In[10]:
+
+
+#print all the even numbers between 1 to 20 
+
+for i in range(1, 21,2):
+      print(i)
+  
+
+
+# In[ ]:
+
+
+#print the squares of number from 1 to 10 
+
+for i in range (1,11):
+    square = i ** 2
+    print(square)
+
+
+# In[ ]:
+
+
+# Square calculator
+while True:
+    value = int(input("Enter the number: "))
+    square = value ** 2
+    print(f"The squared {value} is {square}")
+    break
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
